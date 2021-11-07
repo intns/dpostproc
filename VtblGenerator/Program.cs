12d9f6c4643274fafb132f8e5e4676782fb1d04b
intns <84647527+intns@users.noreply.github.com>
2021-11-07 02:45:05 +0000

    Complete rewrite of VtblGenerator

--- VtblGenerator/Program.cs
+++ VtblGenerator/Program.cs
@@ -6,158 +6,198 @@ using arookas;
 
 namespace VtblGenerator
 {
-	public class Structure
-	{
-		// Name of the structure
-		public string _Name = string.Empty;
-		// Last element is always guaranteed to be the same as _Name
-		public List<string> _QualifiedNames = new();
-		// Virtual functions inside the structure
-		public List<string> _Functions = new();
 
-		public Structure()
+	class Manager
+	{
+		Structure _ParentStruct = new();
+		List<Structure> _DerivativeStructs = new();
+
+		public Manager(string vtblMangled)
 		{
+			_ParentStruct.SetFromVtbl(Demangler.Demangle(vtblMangled).Replace("::__vt", ""));
 		}
 
-		public Structure(string name, List<string> functionsDemangled)
+		// First line of the vtable (excluding the first 2 "0"'s) should have index 0
+		public void ParseLine(string line, int idx)
 		{
-			string[] qualifiers = name.Split("::", StringSplitOptions.RemoveEmptyEntries);
+			if (line == "0")
+			{
+				_ParentStruct._Symbols.Add(new(idx * 4, null, null));
+				return;
+			}
 
-			_Name = qualifiers[^1];
-			_QualifiedNames.AddRange(qualifiers);
-			_Functions.AddRange(functionsDemangled);
+			string dirtyFunc = Demangler.Demangle(line);
+			// Remove the parameters to not confuse the qualifier splitting system
+			string cleanedFunc = dirtyFunc.Substring(0, dirtyFunc.IndexOf("("));
+			List<string> qualifiers = cleanedFunc.Split("::", StringSplitOptions.RemoveEmptyEntries).ToList();
+			qualifiers.RemoveAt(qualifiers.Count - 1);
+
+			if (_ParentStruct._Name == qualifiers[^1])
+			{
+				_ParentStruct._Symbols.Add(new FunctionSymbol(idx * 4, qualifiers, dirtyFunc));
+			}
+			// Ignore thunk functions
+			else if (cleanedFunc.Contains("@"))
+			{
+				return;
+			}
+			else
+			{
+				// If the function isn't from the parent structure, then we can create a new structure
+				// or if the structure exists, add to it
+				bool found = false;
+				foreach (var structure in _DerivativeStructs)
+				{
+					if (structure._Name == qualifiers[^1])
+					{
+						structure._Symbols.Add(new FunctionSymbol(idx * 4, qualifiers, dirtyFunc));
+						found = true;
+						break;
+					}
+				}
+
+				if (!found)
+				{
+					var structure = new Structure();
+					structure._Name = qualifiers[^1];
+
+					for (int i = 0; i < qualifiers.Count - 1; i++)
+					{
+						structure._Qualifiers.Add(qualifiers[i]);
+					}
+
+					structure._Symbols.Add(new FunctionSymbol(idx * 4, qualifiers, dirtyFunc));
+					_DerivativeStructs.Add(structure);
+				}
+
+				_ParentStruct._Symbols.Add(new(idx * 4, qualifiers, dirtyFunc));
+			}
 		}
 
 		public void Output()
 		{
-			// Output the surroundings
-			for (int i = 0; i < _QualifiedNames.Count - 1; i++)
+			foreach (var structure in _DerivativeStructs)
 			{
-				Console.WriteLine($"namespace {_QualifiedNames[i].Trim()} {{ // Unsure if namespace or structure!");
+				OutputStructure(structure);
+			}
+			OutputStructure(_ParentStruct);
+		}
+
+		public static void OutputStructure(Structure structure)
+		{
+			foreach (var qual in structure._Qualifiers)
+			{
+				Console.WriteLine($"namespace {qual} {{");
 			}
 
-			// For padding calculations
-			int maxStringLength = 0;
+			Console.WriteLine($"struct {structure._Name} {{");
 
-			List<string> toPrintFuncs = new();
-			List<string> toPrintComments = new();
-			List<Structure> otherStructures = new();
-
-			for (int i = 0; i < _Functions.Count; i++)
+			// HACK: pad out virtual functions to get the offset generation correctly
+			for (int i = 0; i < structure._Symbols.Count; i++)
 			{
-				string curFunc = _Functions[i].Trim();
-				// Start comment with the offset of the function
-				string commentString = $"// _{i * 4:X2}";
-
-				if (curFunc.Contains(_Name))
+				if (structure._Symbols[i]._Offset != i * 4)
 				{
-					curFunc = curFunc.Replace(_Name + "::", "");
-				}
-
-				// Remove structure qualifiers at the start of the string
-				for (int j = 0; j < _QualifiedNames.Count - 1; j++)
-				{
-					if (curFunc.StartsWith(_QualifiedNames[j]))
-					{
-						commentString += ", from " + curFunc.Substring(0, _QualifiedNames[j].Length + 2).Trim();
-						curFunc = curFunc.Remove(0, _QualifiedNames[j].Length + 2);
-					}
-				}
-
-				// Function is a demangled symbol, and not a pure virtual
-				if (!curFunc.StartsWith("virtual void"))
-				{
-					string noParams = curFunc.Substring(0, curFunc.IndexOf("("));
-					string[] otherQualifiedNames = noParams.Split("::", StringSplitOptions.RemoveEmptyEntries);
-					if (otherQualifiedNames.Length > 1)
-					{
-						bool found = false;
-
-						foreach (Structure structure in otherStructures)
-						{
-							for (int j = 0; j < otherQualifiedNames.Length; j++)
-							{
-								if (structure._Name == otherQualifiedNames[j])
-								{
-									structure._Functions.Add(curFunc);
-									found = true;
-									break;
-								}
-							}
-						}
-
-						if (!found)
-						{
-							Structure structure = new(otherQualifiedNames[0], new() { curFunc });
-							otherStructures.Add(structure);
-						}
-
-						commentString += ", from " + curFunc.Substring(0, otherQualifiedNames[0].Length + 2).Trim();
-						curFunc = curFunc.Remove(0, otherQualifiedNames[0].Length + 2);
-					}
-
-					// Make sure the function is a virtual void and NOT a dtor/ctor
-					bool isDtor = curFunc.StartsWith("~");
-
-					if (!isDtor)
-					{
-						curFunc = $"virtual void {curFunc}";
-					}
-					else
-					{
-						curFunc = $"virtual {curFunc};";
-					}
-				}
-
-				if (maxStringLength < curFunc.Length)
-				{
-					maxStringLength = curFunc.Length;
-				}
-
-				toPrintFuncs.Add("\t" + curFunc);
-				toPrintComments.Add(commentString);
-			}
-
-			foreach (var structure in otherStructures)
-			{
-				structure.Output();
-				Console.WriteLine();
-			}
-
-			Console.Write($"struct {_Name}");
-
-			if (otherStructures.Count != 0)
-			{
-				Console.Write($" : public {otherStructures[0]._Name}");
-				for (int i = 1; i < otherStructures.Count; i++)
-				{
-					Console.Write($", public {otherStructures[i]._Name}");
+					structure._Symbols.Insert(i, new(i * 4, null, null));
 				}
 			}
 
-			Console.WriteLine("\n{");
-			for (int i = 0; i < toPrintFuncs.Count; i++)
+			List<string> symbolString = new();
+			int maxWidth = 0;
+			for (int i = 0; i < structure._Symbols.Count; i++)
 			{
-				Console.Write(toPrintFuncs[i]);
+				FunctionSymbol symbol = structure._Symbols[i];
 
-				// Align the comments
-				for (int j = 0; j < (maxStringLength - toPrintFuncs[i].Length) + 2; j++)
+				string symbolStr = symbol.ToString();
+				if (maxWidth < symbolStr.Length)
+				{
+					maxWidth = symbolStr.Length + 1;
+				}
+				symbolString.Add(symbolStr);
+			}
+
+			for (int i = 0; i < symbolString.Count; i++)
+			{
+				Console.Write($"\t{symbolString[i]}");
+
+				for (int j = 0; j < maxWidth - symbolString[i].Length; j++)
 				{
 					Console.Write(" ");
 				}
-				Console.WriteLine(toPrintComments[i]);
+
+				Console.WriteLine($"// _{structure._Symbols[i]._Offset.ToString("X2").ToUpper()}");
 			}
-			Console.WriteLine("\n\t// _00, VTBL");
 			Console.WriteLine("};");
 
-
-			for (int i = _QualifiedNames.Count - 2; i >= 0; i--)
+			foreach (var qual in structure._Qualifiers)
 			{
-				Console.WriteLine($"}} // namespace {_QualifiedNames[i]}");
+				Console.WriteLine($"}} // namespace {qual}\n");
 			}
 		}
 	}
 
+
+	class Structure
+	{
+		public string _Name;
+		public List<string> _Qualifiers = new List<string>();
+		public List<FunctionSymbol> _Symbols = new List<FunctionSymbol>();
+
+		// Param is demangled vtbl string
+		public void SetFromVtbl(string vtblStr)
+		{
+			string[] qualifiers = vtblStr.Split("::", StringSplitOptions.RemoveEmptyEntries);
+			_Name = qualifiers[^1];
+			for (int i = 0; i < qualifiers.Length - 1; i++)
+			{
+				_Qualifiers.Add(qualifiers[i]);
+			}
+		}
+	}
+
+
+	class FunctionSymbol
+	{
+		public List<string> _Qualifiers = new List<string>();
+		public string _Name = null;
+		public int _Offset = 0;
+		public bool _IsPureVirtual = false;
+
+		public FunctionSymbol(int offset, List<string>? qualifiers, string? name)
+		{
+			if (name != null)
+			{
+				_Name = name;
+			}
+			if (qualifiers != null)
+			{
+				_Qualifiers = qualifiers;
+			}
+			_Offset = offset;
+		}
+
+		public override string ToString()
+		{
+			string output = "virtual ";
+
+			if (_Name != null)
+			{
+				string name = _Name;
+				for (int i = 0; i < _Qualifiers.Count; i++)
+				{
+					name = name.Replace(_Qualifiers[i] + "::", "");
+				}
+				// Normal functions
+				output += "void " + name;
+			}
+			else
+			{
+				// Pure virtual functions
+				output += $"void _{_Offset.ToString("X2").ToUpper()}() = 0";
+			}
+
+			return output + ";";
+		}
+	}
 
 	class Program
 	{
@@ -182,42 +222,22 @@ namespace VtblGenerator
 			// Remove empty lines
 			lines = lines.Where(s => !string.IsNullOrEmpty(s)).ToList();
 
-			List<string> functions = new();
-			bool sameScope = false;
-			string prevScope = "";
+			// Get the class the virtual table is a part of
+			string mainClassName = Demangler.Demangle(lines[0].Replace(":", "")).Replace("::__vt", "");
+			Console.WriteLine(mainClassName);
+
+			// Clean the lines to remove the .4byte
 			for (int i = 0; i < lines.Count; i++)
 			{
-				string line = lines[i].Trim();
-
-				if (!line.StartsWith(".4byte") || (line.StartsWith(".4byte 0") && i < 3))
-				{
-					continue;
-				}
-
-				string symbol = line.Split(" ")[1];
-
-				int vtblOffset = (i - 3) * 4;
-				string demangled;
-				if (symbol != "0")
-				{
-					demangled = Demangler.Demangle(symbol.Replace("\"", ""));
-
-					string scopeStr = demangled.Split("::")[0];
-					sameScope = scopeStr == prevScope;
-					prevScope = scopeStr;
-				}
-				else
-				{
-					demangled = $"virtual void _{vtblOffset:X2}() = 0;";
-				}
-
-				functions.Add(demangled);
+				lines[i] = lines[i].Replace(".4byte", "").Trim();
 			}
 
-			// Create a main structure and output it
-			string rawStr = Demangler.Demangle(lines[0].Replace(":", "")).Replace("__vt", "");
-			new Structure(rawStr, functions)
-				.Output();
+			Manager manager = new Manager(lines[0]);
+			for (int i = 3; i < lines.Count; i++)
+			{
+				manager.ParseLine(lines[i].Replace("\"", ""), i - 3);
+			}
+			manager.Output();
 		}
 	}
 }
